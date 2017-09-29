0c3b1fceeac504ba71d303aa3845c77c9e2dccc4
Lyndon Maydwell <lyndon@sordina.net>
2017-09-29 17:02:05 +1000

    y tho

--- crashkell.md
+++ crashkell.md
@@ -6,16 +6,34 @@ geometry: top=3cm, bottom=4cm, left=2cm, right=2cm
 
 \clearpage
 
-# Chapter 1: Strings
+# Chapter: Strings
 
 ```haskell
 "All about strings!
+```
+
+```why
+Strings must be quoted with an open and close quote.
 ```
 
 ## Exercise: "Hello World"
 
 ```haskell
 prinp "Hello Sworld"
+```
+
+```why
+You can print anything showable to STDOUT using the "print" function.
+```
+
+## Exercise: Quoth the Raven
+
+```haskell
+'Nevermore'
+```
+
+```why
+Strings must be quoted with double-quotes.
 ```
 
 ## Exercise: Parens
@@ -30,6 +48,10 @@ prinp "Hello Sworld"
 print "Hello" ++ "World"
 ```
 
+```why
+Parenthesise your constructed string.
+```
+
 <!--
 ## Exercise: Variables
 
@@ -41,6 +63,48 @@ let
 	in print p
 ```
 -->
+
+# Chapter: Tuples
+
+```haskell
+let (a,b) = (1,2,3) in a * 2 + b * 3
+```
+
+# Chapter: Numbers
+
+```haskell
+1 ? 1 == 2
+```
+
+## Exercise: Multiplication
+
+```haskell
+(2 * 2 + 3, 3 + 2 * 2, (3 + 2) * 2)
+```
+
+# Chapter: Lists
+
+```haskell
+product (1,2,3)
+```
+
+## Exercise: Ranges
+
+```haskell
+[10..1]
+```
+
+## Exercise: Mapping
+
+```haskell
+map succ [1..10]
+```
+
+# Chapter: Lambdas
+
+```haskell
+map (\x -> x+1) [1..10]
+```
 
 
 
--- include/eval.js
+++ include/eval.js
@@ -1,26 +1,42 @@
 
-ddd = null;
 
 function makeResults(elem) {
 	var code    = elem.parent(".sourceCode")
-	var results = code.next(".results")
+	var parent  = code.parent("div.sourceCode")
+	var wrapper = parent.next(".results-wrapper")
+  var results = wrapper.find('.results')
 	if(! results[0]) {
-		results = $("<div class='results'></div>")
-		code.after(results)
+		var wrapper = $("<div class='results-wrapper'></div>")
+		results     = $("<div class='results'></div>")
+		wrapper.append(results)
+		parent.after(wrapper)
 	}
+	results.empty()
+	results.text("Results loading...");
 	return results;
+}
+
+function nextWhy(e) {
+	if(!e) {
+		return;
+	}
+	if(e.prop("tagName").match(/^H/)) {
+		return;
+	}
+	if(e.hasClass('why')) {
+		return e;
+	}
+	return nextWhy(e.next());
 }
 
 function hook(el) {
 	var elem    = $(el);
+	var why     = nextWhy(elem.closest('div.sourceCode'))
 	var results = makeResults(elem);
-	results.empty();
-	results.text("Results loading...");
 
 	return function(data) {
 		console.log(data)
 		data = $.parseJSON(data)
-		ddd  = data;
 
 		results.empty()
 		if(data.success) {
@@ -41,14 +57,25 @@ function hook(el) {
 			}
 		}
 		if(data.error) {
-			var p = $("<pre class='error'></pre>");
-					p.text(data.error);
+			var p      = $("<pre class='error'></pre>");
+			var button = $("<button>y tho</button>");
+			button.css({float: "right"})
+			button.click( function(){
+					why.slideDown('slow');
+					button.hide();
+			});
+			p.text(data.error);
+			if(why && why[0] && ! why.is(':visible')) {
+				console.log('why')
+				console.log(why)
+				p.prepend(button);
+			}
 			results.append(p);
 		}
 	}
 }
 
-function error(elem) {
+function networkError(elem) {
 	return function(data, status, msg) {
 		console.log(data);
 		console.log(status);
@@ -76,7 +103,9 @@ function setup(e){
 	var elem   = $(this);
 	var parent = elem.parent('.sourceCode');
 	var button = $("<button>Run!</button>");
+	var why    = $(".why")
 
+	why.hide()
 	elem.prop('contenteditable', true);
 	elem.on('keydown', tabCatcher(elem, button));
 	parent.prepend( button );
@@ -88,7 +117,7 @@ function setup(e){
 			"/eval",
 			{"exp": text },
 			hook(elem)
-		).fail(error(elem))
+		).fail(networkError(elem))
 	})
 }
 
